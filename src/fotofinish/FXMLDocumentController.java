68ed7e9516fbeb1180e800dfdea0d0da0b208cc4
Lucas Parzianello <lucasparzianello@outlook.com>
2015-10-07 19:28:56 -0400

    Merged [grayscale, sepia, brightness]
    Merged result.
    Features added:
    -> Image loading, functional brightness slider.
    -> New filters working: grayscale and sepia

--- src/fotofinish/FXMLDocumentController.java
+++ src/fotofinish/FXMLDocumentController.java
@@ -1,16 +1,22 @@
 package fotofinish;
-
+import java.awt.image.BufferedImage;
+import java.awt.image.RescaleOp;
 import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Platform;
 import javafx.beans.value.ObservableValue;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.Slider;
@@ -18,11 +24,26 @@ import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Stage;
+import javax.imageio.ImageIO;
 
 public class FXMLDocumentController implements Initializable {
 
     private static final Logger logger = Logger.getLogger(fotofinish.FXMLDocumentController.class.getName());
+    private Label label;
+    public Stage stage;
+    BufferedImage img,original;
     private FotoFinishModel model;
+    @FXML
+    private Button filterGrayscaleButton;
+    @FXML
+    private Button filterSepiaButton;
+    @FXML
+    private Button filterInstantButton;
+    @FXML
+    private Button filterCustomButton;
+    @FXML
+    private Button filterNoneButton;
     @FXML
     private Slider brightnessSlider;
     @FXML
@@ -40,11 +61,46 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private MenuItem menubarHelpAboutMenuItem;
     @FXML
+    private Label filtersLabel;
+    @FXML
+    private Label sliderLabel;
+    @FXML
+    private Label brightnessLabel;
+    @FXML
+    private Label contrastLabel;
+    @FXML
+    private Label drawingLabel;
+    @FXML
     private ColorPicker brushColorPicker;
     @FXML
     private NumberFieldFX brushSizeNumberField;
     @FXML
-    private ImageView imageViewer;
+    private Label brushTypeLabel;
+    @FXML
+    private Label brushSizeLabel;
+    @FXML
+    private NumberFieldFX brushSizeTextField;
+    @FXML
+    private MenuItem menubarFileNew;
+    @FXML
+    private MenuItem menubarFileOpen;
+    @FXML
+    private MenuItem menubarFileGalleryButterfly;
+    @FXML
+    private MenuItem menubarFileGalleryTeddyBear;
+    @FXML
+    private MenuItem menubarFileGalleryPrincess;
+    @FXML
+    private MenuItem menubarFileGalleryFirefighter;
+    @FXML
+    private MenuItem menubarFileSave;
+    @FXML
+    private MenuItem menubarFileSaveAs;
+    @FXML
+    private MenuItem menubarFileQuit;
+    @FXML
+    public ImageView imageViewer;
+    public File selectedFile;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -52,13 +108,25 @@ public class FXMLDocumentController implements Initializable {
 
         //TODO: make call function only when value changes by certain threshold
         this.brightnessSlider.valueProperty().addListener((ObservableValue<? extends Number> observable, Number oldValue, Number newValue) -> {
-            if(model.changeBrightness(newValue)) {
+            /*if(model.changeBrightness(newValue)) {
                 this.refreshImageViewer();
+            }*/
+            System.out.println("TODO: brightness changed to " + newValue);
+            float brilho = (newValue.floatValue()) ;
+            if ( 0 >= brilho) {
+                brilho = (100+brilho);
+                brilho = brilho/100;
             }
+            else brilho = 1+ brilho/100;
+            System.out.println(brilho);
+            RescaleOp op = new RescaleOp(brilho, 0, null);
+            op.filter(original, img);
+            imageViewer.setImage(SwingFXUtils.toFXImage(img, null));
         });
 
         this.contrastSlider.valueProperty().addListener((ObservableValue<? extends Number> observable, Number oldValue, Number newValue) -> {
-            if(model.changeContrast(newValue)) {
+            System.out.println("TODO: contrast changed to " + newValue);
+            if (model.changeContrast((int) newValue)) {
                 this.refreshImageViewer();
             }
         });
@@ -66,24 +134,30 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private void filterGrayscale(ActionEvent ignored) {
-        model.applyGrayscaleFilter();
-        this.refreshImageViewer();
+        System.out.println("TODO: grayscale filter applied");
+        imageViewer.setImage(model.applyGrayscaleFilter(img));
+        /*model.applyGrayscaleFilter();
+        this.refreshImageViewer();*/
     }
 
     @FXML
     private void filterSepia(ActionEvent ignored) {
-        model.applySepiaFilter();
-        this.refreshImageViewer();
+        System.out.println("TODO: sepia filter applied");
+        imageViewer = model.applySepiaFilter(imageViewer);
+        /*model.applySepiaFilter();
+        this.refreshImageViewer();*/
     }
 
     @FXML
-    private void filterInstant(ActionEvent ignored) {
+    private void filterInstant(ActionEvent event) {
+        System.out.println("TODO: instant filter applied");
         model.applyInstantFilter();
         this.refreshImageViewer();
     }
 
     @FXML
     private void filterCustom(ActionEvent ignored) {
+        System.out.println("TODO: launched custom filter popup");
         //TODO: code to create custom filter popup and get arguments for custom filter
         model.applyCustomFilter();
         this.refreshImageViewer();
@@ -91,45 +165,54 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private void filterNone(ActionEvent ignored) {
-        model.reloadImage();
+        System.out.println("TODO: none filter applied");
+        imageViewer.setImage(SwingFXUtils.toFXImage(original, null));
+        // model.reloadImage();
         this.refreshImageViewer();
     }
 
     @FXML
     private void brushTypeCircle(ActionEvent ignored) {
+        System.out.println("TODO: brush type changed to circle");
         model.setBrushTypeCircle();
     }
 
     @FXML
     private void brushTypeSquare(ActionEvent ignored) {
+        System.out.println("TODO: brush type changed to square");
         model.setBrushTypeSquare();
     }
 
     @FXML
     private void brushTypeSpraypaint(ActionEvent ignored) {
+        System.out.println("TODO: brush type changed to spraypaint");
         model.setBrushTypeSpraypaint();
     }
 
     @FXML
     private void helpDoc(ActionEvent ignored) {
+         System.out.println("TODO: launched help document");
     }
 
     @FXML
     private void aboutDialog(ActionEvent ignored) {
+        System.out.println("TODO: created about dialog");
     }
 
     @FXML
     private void brushColor(ActionEvent ignored) {
+        System.out.println("TODO: brush color changed to <BRUSH COLOR>");
         model.changeBrushColor(this.brushColorPicker.getValue());
     }
 
     @FXML
     private void brushSize(ActionEvent ignored) {
+        System.out.println("TODO: brush size changed to <BRUSH SIZE>");
         model.changeBrushSize(Integer.parseInt(this.brushSizeNumberField.getText()));
     }
 
     @FXML
-    private void open(ActionEvent ignored) {
+    private void open(ActionEvent ignored) throws MalformedURLException, IOException {
         logger.log(Level.INFO, "open file chooser launched");
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open Image");
@@ -137,8 +220,13 @@ public class FXMLDocumentController implements Initializable {
         File selectedFile = fileChooser.showOpenDialog(null); //TODO: needs value in order to block main window
         if (selectedFile != null) {
             logger.log(Level.INFO, "file {0} choosen in open file chooser", selectedFile);
+            img = ImageIO.read(selectedFile);
+            original = ImageIO.read(selectedFile);
+            imageViewer.setImage(SwingFXUtils.toFXImage(img, null));
+            /* OLD IMAGE LOADING CODE:
             model.loadImage(selectedFile);
             this.refreshImageViewer();
+            */
         } else {
             logger.log(Level.INFO, "no file selected in open file chooser");
         }
@@ -148,24 +236,28 @@ public class FXMLDocumentController implements Initializable {
     private void galleryButterfly(ActionEvent ignored) {
         model.loadGalleryButterflyImage();
         this.refreshImageViewer();
+        // TODO: opened butterfly file from gallery"
     }
 
     @FXML
     private void galleryTeddyBear(ActionEvent ignored) {
         model.loadGalleryTeddyBearImage();
         this.refreshImageViewer();
+        // TODO: opened teddy bear file from gallery
     }
 
     @FXML
     private void galleryPrincess(ActionEvent ignored) {
         model.loadGalleryPrincessImage();
         this.refreshImageViewer();
+        // TODO: opened princess file from gallery
     }
 
     @FXML
     private void galleryFirefighter(ActionEvent ignored) {
         model.loadGalleryFirefighterImage();
         this.refreshImageViewer();
+        // TODO: opened firefighter file from gallery
     }
 
     @FXML
@@ -188,7 +280,7 @@ public class FXMLDocumentController implements Initializable {
             logger.log(Level.INFO, "no file selected in save as file chooser");
         }
     }
-
+    
     @FXML
     private void quit(ActionEvent ignored) {
         logger.log(Level.INFO, "quitting");
@@ -196,7 +288,8 @@ public class FXMLDocumentController implements Initializable {
     }
 
     @FXML
-    private void newFile(ActionEvent ignored) {
+    private void newFile(ActionEvent event) {
+        System.out.println("TODO: created new file");
     }
 
     private void refreshImageViewer() {
--- src/fotofinish/FotoFinishModel.java
+++ src/fotofinish/FotoFinishModel.java
@@ -1,5 +1,6 @@
 package fotofinish;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -7,12 +8,16 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.effect.SepiaTone;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javax.imageio.ImageIO;
 
 public class FotoFinishModel {
+
     private enum BrushType {
+
         CIRCLE,
         SQUARE,
         SPRAYPAINT
@@ -24,16 +29,16 @@ public class FotoFinishModel {
     private BrushType brushType;
     private int brushSize;
     private Color brushColor;
-    private int brightness;
-    private int contrast;
+    private int currBrightness;
+    private int currContrast;
     private final int SLIDER_CHANGE_THRESH = 1;
 
     //TODO: how can we better synchronize this with FXML?
     public FotoFinishModel() {
         this.brushType = BrushType.CIRCLE;
         this.brushSize = 10;
-        this.brightness = 0;
-        this.contrast = 0;
+        currBrightness = 50;
+        currContrast = 50;
     }
 
     public void loadImage(File file) {
@@ -44,10 +49,10 @@ public class FotoFinishModel {
     public void reloadImage() {
         if (this.imageFile != null) {
             try {
-                this.image = new Image(new FileInputStream(this.imageFile));
+                this.image = new Image(new FileInputStream(imageFile));
                 logger.log(Level.INFO, "loaded file {0} as image", this.imageFile);
             } catch (FileNotFoundException fnfex) {
-                logger.log(Level.SEVERE, "error loading file {0} as image{1}", new Object[] {this.imageFile, fnfex});
+                logger.log(Level.SEVERE, "error loading file {0} as image{1}", new Object[]{this.imageFile, fnfex});
             }
         }
     }
@@ -62,7 +67,7 @@ public class FotoFinishModel {
             ImageIO.write(SwingFXUtils.fromFXImage(image, null), "jpg", file);
             logger.log(Level.INFO, "saved image as file {0}", file);
         } catch (IOException ioex) {
-            logger.log(Level.SEVERE, "error saving image as file {0}", new Object[] {file, ioex});
+            logger.log(Level.SEVERE, "error saving image as file {0}", new Object[]{file, ioex});
         }
     }
 
@@ -70,14 +75,27 @@ public class FotoFinishModel {
         return this.image;
     }
 
-    public void applyGrayscaleFilter() {
-        //TODO: code to apply grayscale filter to image
-        logger.log(Level.INFO, "applied grayscale filter");
+    public Image applyGrayscaleFilter(BufferedImage img) {
+        for (int x = 0; x < img.getWidth(); ++x) {
+            for (int y = 0; y < img.getHeight(); ++y) {
+                int rgb = img.getRGB(x, y);
+                int r = (rgb >> 16) & 0xFF;
+                int g = (rgb >> 8) & 0xFF;
+                int b = (rgb & 0xFF);
+
+                int grayLevel = (r + g + b) / 3;
+                int gray = (grayLevel << 16) + (grayLevel << 8) + grayLevel;
+                img.setRGB(x, y, gray);
+            }
+        }
+        Image image = SwingFXUtils.toFXImage(img, null);
+        logger.log(Level.INFO, "Applied grayscale filter");
+        return image;
     }
 
-    public void applySepiaFilter() {
-        //TODO: code to apply sepia filter to image
-        logger.log(Level.INFO, "applied sepia filter");
+    public ImageView applySepiaFilter(ImageView imagev) {
+        imagev.setEffect(new SepiaTone());
+        return imagev;
     }
 
     public void applyInstantFilter() {
@@ -92,10 +110,10 @@ public class FotoFinishModel {
 
     public boolean changeBrightness(Number newBrightnessNumber) {
         int newBrightness = newBrightnessNumber.intValue();
-        if (Math.abs(this.brightness - newBrightness) >= this.SLIDER_CHANGE_THRESH) {
-            //TODO: code to adjust brightness
-            logger.log(Level.INFO, "brightness changed from {0} to {1}", new Object[] {newBrightness, this.brightness});
-            this.brightness = newBrightness;
+        if (Math.abs(this.currBrightness - newBrightness) >= this.SLIDER_CHANGE_THRESH) {
+            //TODO: code to adjust currBrightness
+            logger.log(Level.INFO, "brightness changed from {0} to {1}", new Object[]{newBrightness, this.currBrightness});
+            this.currBrightness = newBrightness;
             return true;
         }
         return false;
@@ -103,10 +121,10 @@ public class FotoFinishModel {
 
     public boolean changeContrast(Number newContrastNumber) {
         int newContrast = newContrastNumber.intValue();
-        if (Math.abs(this.contrast - newContrast) >= this.SLIDER_CHANGE_THRESH) {
+        if (Math.abs(this.currContrast - newContrast) >= this.SLIDER_CHANGE_THRESH) {
             //TODO: code to adjust contrast
-            logger.log(Level.INFO, "contrast changed from {0} to {1}", new Object[] {newContrast, this.contrast});
-            this.contrast = newContrast;
+            logger.log(Level.INFO, "contrast changed from {0} to {1}", new Object[]{newContrast, this.currContrast});
+            this.currContrast = newContrast;
             return true;
         }
         return false;
